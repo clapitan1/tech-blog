6b7a2b57e9872faf07bcce863282cd12d190e7ef
clapitan1 <cilapitan1@gmail.com>
2023-05-08 20:43:02 -0700

    app use

--- server.js
+++ server.js
@@ -6,9 +6,34 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
 const { Model } = require('sequelize');
+const { urlencoded } = require('body-parser');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+});
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sessionOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
