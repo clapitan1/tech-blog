6020251d6475b21763ef4ec868e9c789dbe89377
clapitan1 <cilapitan1@gmail.com>
2023-05-08 21:20:57 -0700

    server.js

--- server.js
+++ server.js
@@ -37,3 +37,113 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3001,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySql database');
+});
+
+class User extends Model {}
+
+User.init(
+    {
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'user',
+    }
+);
+
+class Post extends Model {}
+
+Post.init(
+    {
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        contents: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'post',
+    }
+);
+
+User.hasMany(Post, { foreignKey: 'userId' });
+Post.belongsTo(User, { foreignKey: 'userId' });
+
+app.get('/', (req, res) => {
+    Post.findAll({
+        include: User,
+        order: [['createdAt', 'DESC']],
+    })
+        .then((posts) => {
+            res.render('homepage', { posts });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get('/login', (req, res) => {
+    res.render('/login');
+});
+
+app.post('/login', (req, res) => {
+    const {username, password } = req.body;
+
+    User.findOne({ where: { username } })
+    .then((user) => {
+        if (!user) {
+            res.status(400).send('Invalid username or password');
+        } else {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (err || !result) {
+                    res.status(400).send('Invalid username or password');
+                } else {
+                    req.session.user = user;
+                    res.redirect('/');
+                }
+            });
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+app.get('/logout' (req, res => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+}));
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}');
+});
